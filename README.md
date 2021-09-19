409a7cfaef469e071f5e1c3578d6b4aa3a8252ea
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2021-09-19 06:54:07 +0200

    kata street fighter

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # CodeWars Katas
-Solutions to exercises
+Solutions to katas while doing the Ironhack bootcamp.
+
+# TO-DO
+Explanation to my own katas in case anyone is interested.
+
+```
+Plz, try to understand, don't just copy/paste it.
+```
---
+++ javascript/6kyu/street_fighter_2.js
@@ -0,0 +1,223 @@
+/*
+Short Intro
+
+Some of you might remember spending afternoons playing Street Fighter 2 in some Arcade back in the 90s or emulating 
+it nowadays with the numerous emulators for retro consoles.
+
+Can you solve this kata? Suuure-You-Can!
+
+UPDATE: a new kata's harder version is available here.
+
+The Kata
+
+You'll have to simulate the video game's character selection screen behaviour, more specifically the selection grid.
+Such screen looks like this:
+
+Screen:
+
+Selection Grid Layout in text:
+
+| Ryu  | E.Honda | Blanka  | Guile   | Balrog | Vega    |
+| Ken  | Chun Li | Zangief | Dhalsim | Sagat  | M.Bison |
+Input
+
+the list of game characters in a 2x6 grid;
+the initial position of the selection cursor (top-left is (0,0));
+a list of moves of the selection cursor (which are up, down, left, right);
+Output
+
+the list of characters who have been hovered by the selection cursor after all the moves (ordered and with repetition, 
+	all the ones after a move, wether successful or not, see tests);
+Rules
+
+Selection cursor is circular horizontally but not vertically!
+
+As you might remember from the game, the selection cursor rotates horizontally but not vertically; that means that if I'm in 
+the leftmost and I try to go left again I'll get to the rightmost (examples: from Ryu to Vega, from Ken to M.Bison) and vice 
+versa from rightmost to leftmost.
+
+Instead, if I try to go further up from the upmost or further down from the downmost, I'll just stay where I am located (examples: 
+	you can't go lower than lowest row: Ken, Chun Li, Zangief, Dhalsim, Sagat and M.Bison in the above image; you can't go upper 
+	than highest row: Ryu, E.Honda, Blanka, Guile, Balrog and Vega in the above image).
+
+Test
+
+For this easy version the fighters grid layout and the initial position will always be the same in all tests, only the list of moves change.
+
+Notice: changing some of the input data might not help you.
+
+Examples
+
+1.
+fighters = [
+    ["Ryu", "E.Honda", "Blanka", "Guile", "Balrog", "Vega"],
+    ["Ken", "Chun Li", "Zangief", "Dhalsim", "Sagat", "M.Bison"]
+]
+initial_position = (0,0)
+moves = ['up', 'left', 'right', 'left', 'left']
+then I should get:
+
+['Ryu', 'Vega', 'Ryu', 'Vega', 'Balrog']
+as the characters I've been hovering with the selection cursor during my moves. Notice: Ryu is the first just because it
+"fails" the first up See test cases for more examples.
+
+2.
+fighters = [
+    ["Ryu", "E.Honda", "Blanka", "Guile", "Balrog", "Vega"],
+    ["Ken", "Chun Li", "Zangief", "Dhalsim", "Sagat", "M.Bison"]
+]
+initial_position = (0,0)
+moves = ['right', 'down', 'left', 'left', 'left', 'left', 'right']
+Result:
+
+['E.Honda', 'Chun Li', 'Ken', 'M.Bison', 'Sagat', 'Dhalsim', 'Sagat']
+OFF-TOPIC
+
+Some music to get in the mood for this kata :
+
+Street Fighter 2 - selection theme
+
+
+*/
+function streetFighterSelection(fighters, position, moves) {
+	var result = [];
+
+	moves.forEach(function(move) {
+		if (move === 'up') {
+			position[0] = 0;
+		} else if (move === 'down') {
+			position[0] = 1;
+		} else if (move === 'right') {
+			position[1] = position[1] === 5 ? 0 : position[1] + 1;
+		} else if (move === 'left') {
+			position[1] = position[1] === 0 ? 5 : position[1] - 1;
+		}
+
+		result.push(fighters[position[0]][position[1]]);
+	});
+
+	return result;
+}
+// Other results
+//-------------------------------------------------------------
+/* #1
+function streetFighterSelection(fighters, position, moves) {
+	let result = [];
+	let currentPosition = position;
+
+	for (let move of moves) {
+		// This can be a switch for sure
+		if (move == 'up') {
+			if (currentPosition[0] == 0) {
+				result.push(fighters[currentPosition[0]][currentPosition[1]]);
+			} else {
+				currentPosition[0]--;
+				result.push(fighters[currentPosition[0]][currentPosition[1]]);
+			}
+		}
+
+		if (move == 'down') {
+			if (currentPosition[0] == 1) {
+				result.push(fighters[currentPosition[0]][currentPosition[1]]);
+			} else {
+				currentPosition[0]++;
+				result.push(fighters[currentPosition[0]][currentPosition[1]]);
+			}
+		}
+
+		if (move == 'left') {
+			if (currentPosition[1] == 0) {
+				currentPosition[1] = 5;
+				result.push(fighters[currentPosition[0]][currentPosition[1]]);
+			} else {
+				currentPosition[1]--;
+				result.push(fighters[currentPosition[0]][currentPosition[1]]);
+			}
+		}
+
+		if (move == 'right') {
+			if (currentPosition[1] == 5) {
+				currentPosition[1] = 0;
+				result.push(fighters[currentPosition[0]][currentPosition[1]]);
+			} else {
+				currentPosition[1]++;
+				result.push(fighters[currentPosition[0]][currentPosition[1]]);
+			}
+		}
+	}
+
+	return result;
+}
+*/
+/* #2
+function streetFighterSelection(fighters, position, moves){
+
+  const boundY = fighters.length - 1;
+  const boundX = fighters[0].length - 1;
+
+  let [y, x] = position;
+
+  const controls = {
+    right: () => x === boundX ? x = 0 : x++,
+    left: () => x ? x-- : x = boundX,
+    up: () => y ? y-- : y,
+    down: () => y < boundY ? y++ : y
+  };
+  
+  return moves.map(move => {
+    controls[move]();
+    return fighters[y][x];
+  });
+}
+*/
+/* #3
+function streetFighterSelection(fighters, position, moves){
+  return moves.map((pos) => {
+      switch(pos) {
+          case "up":
+              return fighters[ position[0] = 0 ][position[1]]
+              break
+          case "down":
+              return fighters[ position[0] = 1 ][position[1]]
+              break
+          case "left":
+              return fighters[position[0]][(position[1] | 0) ? position[1] -= 1 : position[1] = 5]
+              break
+          case "right":
+              return fighters[position[0]][(position[1] === 5) ? position[1] = 0 : position[1] += 1]
+              break
+      }
+  })
+}
+*/
+/* #4
+function streetFighterSelection(fighters, position, moves){
+  var res = [], i = position[0], j = position[1];
+  for (var k = 0; k < moves.length; k++) {
+      if (moves[k] === 'up')
+         i = i === 1 ? 0 : i;
+      else if (moves[k] === 'down')
+         i = i === 0 ? 1 : i;
+      else if (moves[k] === 'left')
+         j = j === 0 ? fighters[0].length - 1 : j - 1;
+      else if (moves[k] === 'right')
+         j = j === fighters[0].length - 1 ? 0 : j + 1;
+      res.push(fighters[i][j]);
+  }
+  return res;
+}
+*/
+
+fighters = [
+	[ 'Ryu', 'E.Honda', 'Blanka', 'Guile', 'Balrog', 'Vega' ],
+	[ 'Ken', 'Chun Li', 'Zangief', 'Dhalsim', 'Sagat', 'M.Bison' ]
+];
+
+moves = [ 'up', 'left', 'right', 'left', 'left' ];
+console.log(streetFighterSelection(fighters, [ 0, 0 ], moves));
+
+moves = [ 'left', 'left', 'left', 'left', 'left', 'left', 'left', 'left' ];
+console.log(streetFighterSelection(fighters, [ 0, 0 ], moves));
+
+moves = [ 'right', 'right', 'right', 'right', 'right', 'right', 'right', 'right' ];
+console.log(streetFighterSelection(fighters, [ 0, 0 ], moves));
