d67473693e1e14590f0a5e6f5aa3f42b3acd619d
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2021-11-02 02:58:37 +0100

    Two simple katas done

---
+++ javascript/8kyu/string_repeat.js
@@ -0,0 +1,16 @@
+// Write a function called repeatStr which repeats the given string string exactly n times.
+
+// repeatStr(6, "I") // "IIIIII"
+// repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+
+function repeatStr(n, s) {
+	let str = '';
+	for (let index = 0; index < n; index++) {
+		str += s;
+	}
+	return str;
+}
+
+console.log(repeatStr(3, '*'));
+console.log(repeatStr(5, '#'));
+console.log(repeatStr(2, 'ha '));
