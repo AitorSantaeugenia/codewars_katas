59585d4911bdee629e33fdb490ec90b73fb4190a
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2022-03-01 00:58:39 +0100

    added other solutions

--- javascript/4kyu/sum_strings_As_numbers.js
+++ javascript/4kyu/sum_strings_As_numbers.js
@@ -16,11 +16,40 @@ console.log(sumStrings('123', '456')); //'579'
 //----------------------------------------------------------------
 /* ### Other solutions
 #1
-
+function sumStrings(a, b) {
+  var res = '', c = 0;
+  a = a.split('');
+  b = b.split('');
+  while (a.length || b.length || c) {
+    c += ~~a.pop() + ~~b.pop();
+    res = c % 10 + res;
+    c = c > 9;
+  }
+  return res.replace(/^0+/, '');
+}
 
 #2
+function sumStrings(a, b)
+{
+    var A = a.split(""), B = b.split(""), C = 0, R = "";
 
+    while (A.length || B.length || C)
+    {
+        C = C + (~~A.pop() + ~~B.pop());
+        R = (C % 10) + R;
+        C = C > 9;
+    }
+
+    return R.replace(/^0+/, "");
+}
 
 #3
+const BigNumber = require("bignumber.js")
 
+BigNumber.config({
+  EXPONENTIAL_AT: [-100, 100]
+})
+
+const sumStrings = (a,b) =>
+  new BigNumber(a || 0).plus(new BigNumber(b || 0)).toString()
 */
