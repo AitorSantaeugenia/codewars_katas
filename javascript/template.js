ecf8cbc9571767f57794e8b94a3fa5f02a767214
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2022-04-06 19:40:21 +0200

    template added

---
+++ javascript/template.js
@@ -0,0 +1,26 @@
+/* ### TITLE
+
+EXPLANATION OF WHAT TO DO
+
+*/
+
+function yourfunction(str) {
+	// your code
+}
+
+console.log(yourfunction(str)); //expected result
+console.log(yourfunction(str)); //expected result
+console.log(yourfunction(str)); //expected result
+console.log(yourfunction(str)); //expected result
+
+/* Other solutions
+#1
+
+
+#2
+
+
+#3
+
+
+*/
